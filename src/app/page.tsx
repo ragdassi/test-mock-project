f6f34a978319bd0485071f27fec55ab13d6b3e1c
Ryan Agdassi <113940065+ragdassi@users.noreply.github.com>
2024-09-21 15:04:31 -0700

    feat: my about page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,40 @@
-import Navbar from "@components/Navbar";
+import styles from './Home.module.css';
 
 export default function Home() {
   return (
-    <main>
-      <Navbar />
-      <h1>Home</h1>
-    </main>
+    <div className={styles.main}>
+      <div className={styles.container}>
+        <img src="https://media.licdn.com/dms/image/v2/D5603AQFcya7jebNo2g/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1724012519952?e=2147483647&v=beta&t=eTR6xwLbsXt6cUCogxfQUG1pujj0Gub6ns2h_sl4aXI" alt="Ryan Agdassi" className={styles.profilePic} />
+
+        <h1 className={styles.name}>Ryan Agdassi</h1>
+        <h2 className={styles.title}>Tech Advisor at Hack4Impact-Cal Poly</h2>
+
+        <section className={styles.section}>
+          <h3 className={styles.sectionTitle}>About Me</h3>
+          <p className={styles.about}>
+            Ryan Agdassi is a computer science student at California Polytechnic State University - San Luis Obispo studying Computer Science. He is an avid Warriors and Raiders fan.
+          </p>
+        </section>
+
+        <section className={styles.section}>
+          <h3 className={styles.sectionTitle}>Experience</h3>
+          <ul className={styles.list}>
+            <li>Tech Advisor - Hack4Impact</li>
+            <li>Tech Lead - Hack4Impact, Go See Foundation</li>
+            <li>Software Developer - Hack4Impact, Partners in Equestrian Therapy</li>
+          </ul>
+        </section>
+
+        <section className={styles.section}>
+          <h3 className={styles.sectionTitle}>Skills</h3>
+          <ul className={styles.list}>
+            <li>JavaScript, TypeScript</li>
+            <li>React, Next.js, Node.js</li>
+            <li>Python, Java, C</li>
+            <li>MongoDB, PostgreSQL</li>
+          </ul>
+        </section>
+      </div>
+    </div>
   );
 }
