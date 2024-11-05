346a2046e4eb3e1f6bc02deda7ef0161c459ab00
Jason <Jason1@jelincic.com>
2024-11-05 10:27:57 +0800

    feat: some changes to the profile page to make it mine ahahahah

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,24 +4,23 @@ export default function Home() {
   return (
     <div className={styles.main}>
       <div className={styles.container}>
-        <img src="https://media.licdn.com/dms/image/v2/D5603AQFcya7jebNo2g/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1724012519952?e=2147483647&v=beta&t=eTR6xwLbsXt6cUCogxfQUG1pujj0Gub6ns2h_sl4aXI" alt="Ryan Agdassi" className={styles.profilePic} />
+        <img src='/RandomSelfie.jpeg' alt="Jason Jelincic" className={styles.profilePic} />
 
-        <h1 className={styles.name}>Ryan Agdassi</h1>
-        <h2 className={styles.title}>Tech Advisor at Hack4Impact-Cal Poly</h2>
+        <h1 className={styles.name}>Jason Jelincic</h1>
+        <h2 className={styles.title}>Tech Lead at Hack4Impact-Cal Poly</h2>
 
         <section className={styles.section}>
           <h3 className={styles.sectionTitle}>About Me</h3>
           <p className={styles.about}>
-            Ryan Agdassi is a computer science student at California Polytechnic State University - San Luis Obispo. 
+            Jason Jelincic is a computer science student at California Polytechnic State University - San Luis Obispo. 
           </p>
         </section>
 
         <section className={styles.section}>
           <h3 className={styles.sectionTitle}>Experience</h3>
           <ul className={styles.list}>
-            <li>Tech Advisor - Hack4Impact</li>
-            <li>Tech Lead - Hack4Impact, Go See Foundation</li>
-            <li>Software Developer - Hack4Impact, Partners in Equestrian Therapy</li>
+            <li>Tech Lead - Hack4Impact</li>
+            <li>Software Developer - Hack4Impact, SLO Beaver Briage</li>
           </ul>
         </section>
 
